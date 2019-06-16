d47592a134bc55877763586bcecdfeb267b81bdc
vanessa lopez <vannyv09@yahoo.com>
2019-06-16 17:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = [
   'Milo',
   'Otis',
   'Garfield'
-  ]; //define your array here
+  ] //define your array here
 
-function destructivelyAppendKitten(Ralph);
+  function destructivelyAppendKitten(Ralph);
   
